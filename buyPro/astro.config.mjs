c9e8636beba049fb11632ec7061a125abe288a5b
shifat514 <shifat514@gmail.com>
2024-05-09 14:52:43 +0600

    feat: install and configure cloudfare adapter

--- buyPro/astro.config.mjs
+++ buyPro/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import cloudflare from '@astrojs/cloudflare'
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: "hybrid",
+  adapter: cloudflare({
+    imageService: 'cloudflare'
+  })
 });
