eab8921957c52975643947255d9940e7cfa611a5
shifat514 <shifat514@gmail.com>
2024-05-09 15:24:26 +0600

    change output

--- buyPro/astro.config.mjs
+++ buyPro/astro.config.mjs
@@ -6,6 +6,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: "hybrid",
+  output: 'server',
   adapter: cloudflare()
 });
